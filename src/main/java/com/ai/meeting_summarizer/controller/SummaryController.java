816edfffa6dd3262dbe0527a1defc2c8a398f556
Mohd Afzal <afzalmohd44099@gmail.com>
2025-09-02 13:12:21 +0530

    deployment-ready

--- src/main/java/com/ai/meeting_summarizer/controller/SummaryController.java
+++ src/main/java/com/ai/meeting_summarizer/controller/SummaryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://mohdafzal-lumio-frontend-production.up.railway.app/"
+        "https://internship-assignment-mangodesk-san-one.vercel.app/"
 })
 
 
